97431f717b60adb3622057da3223eec9d70ce2e3
Micheal Waldrup <waldrupm@gmail.com>
2019-09-07 15:34:00 -0400

    Finish Unicafe step3

--- src/index.js
+++ src/index.js
@@ -1,57 +1,82 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
+const FeedbackButton = ({onClick, text}) => {
+
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Feedback = ({onGood, onNeutral, onBad}) => {
 
   return (
     <div>
-      button press history: {props.allClicks.join(' ')}
+      <h1>Give Feedback</h1>
+      <FeedbackButton onClick={() => onGood()} text={'Good'}/>
+      <FeedbackButton onClick={() => onNeutral()} text={'Neutal'}/>
+      <FeedbackButton onClick={() => onBad()} text={'Bad'}/>
+
     </div>
   )
 }
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
+const Statistic = (props) => {
+  return (
+    <p>{props.label} {props.value}</p>
+  )
+}
 
-const App = (props) => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+const Statistics = ({good, neutral, bad}) => {
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+  
+  const totalFeedback = () => {
+    return good + neutral + bad;
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const avg = () => {
+    const goodScore = good || 0;
+    const badScore = -bad || 0;
+    if(!goodScore && !badScore) return 0;
+    return (goodScore + badScore) / totalFeedback();
   }
 
   return (
     <div>
-      <div>
-        {left}
-        <Button onClick={handleLeftClick} text='left' />
-        <Button onClick={handleRightClick} text='right' />
-        {right}
-        <History allClicks={allClicks} />
-      </div>
+      <h1>Statistics</h1>
+      <Statistic label={'Good:'} value={good} />
+      <Statistic label={'Neutral:'} value={neutral} />
+      <Statistic label={'Bad:'} value={bad} />
+      <Statistic label={'Total Feedback:'} value={totalFeedback()} />
+      <Statistic label={'Average:'} value={avg()} />
     </div>
   )
 }
 
-ReactDOM.render(
-  <App />, 
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGood = () => {
+    setGood(good + 1);
+  }
+  const addNeutral = () => {
+    setNeutral(neutral + 1);
+  }
+  const addBad = () => {
+    setBad(bad + 1);
+  }
+
+  return (
+    <div>
+      <Feedback onGood={() => addGood()} onNeutral={()=> addNeutral()} onBad={()=> addBad()} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
 )
