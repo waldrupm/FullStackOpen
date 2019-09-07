e4dde7aba0a46f78677a4eacb7251c4cfc76fec2
Micheal Waldrup <waldrupm@gmail.com>
2019-09-07 14:50:17 -0400

    Finish P1d, start exercises

--- src/index.js
+++ src/index.js
@@ -1,30 +1,53 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
-
-const Button = ({onClick, text}) => {
-  return (
-    <button onClick={onClick}>{text}</button>
-  )
-}
-
-const App = (props) => {
-  const [ counter, setCounter ] = useState(0);
-  const setToValue = (value) => setCounter(value);
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={() => setToValue(counter+1)} text={'Plus'} />
-      <Button onClick={() => setToValue(counter-1)} text={'Minus'} />
-      <Button onClick={() => setToValue(0)} text={'Reset'} />
+      button press history: {props.allClicks.join(' ')}
     </div>
+  )
+}
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left' />
+        <Button onClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
   )
 }
 
