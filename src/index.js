f1ef1fd53888e0b194b1857896b9ba25f91d7482
Micheal Waldrup <waldrupm@gmail.com>
2019-09-08 21:39:41 -0400

    Continue notes app P2b

--- src/index.js
+++ src/index.js
@@ -1,83 +1,24 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import Note from './components/Note'
 
-const FeedbackButton = ({onClick, text}) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
 
-  return (
-    <button onClick={onClick}>{text}</button>
+  const rows = () => notes.map(note =>
+    <Note
+      key={note.id}
+      note={note}
+    />
   )
-}
-
-const Feedback = ({onGood, onNeutral, onBad}) => {
 
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <FeedbackButton onClick={() => onGood()} text={'Good'}/>
-      <FeedbackButton onClick={() => onNeutral()} text={'Neutal'}/>
-      <FeedbackButton onClick={() => onBad()} text={'Bad'}/>
-
+      <h1>Notes</h1>
+      <ul>
+        {rows()}
+      </ul>
     </div>
   )
 }
 
-const Statistic = (props) => {
-  return (
-    <p>{props.label} {props.value}</p>
-  )
-}
-
-const Statistics = ({good, neutral, bad}) => {
-
-  
-  const totalFeedback = () => {
-    return good + neutral + bad;
-  }
-
-  const avg = () => {
-    const goodScore = good || 0;
-    const badScore = -bad || 0;
-    if(!goodScore && !badScore) return 0;
-    return (goodScore + badScore) / totalFeedback();
-  }
-
-  if(totalFeedback() === 0) return <p>No feedback given.</p>
-  return (
-    <div>
-      <h1>Statistics</h1>
-      <Statistic label={'Good:'} value={good} />
-      <Statistic label={'Neutral:'} value={neutral} />
-      <Statistic label={'Bad:'} value={bad} />
-      <Statistic label={'Total Feedback:'} value={totalFeedback()} />
-      <Statistic label={'Average:'} value={avg()} />
-    </div>
-  )
-}
-
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const addGood = () => {
-    setGood(good + 1);
-  }
-  const addNeutral = () => {
-    setNeutral(neutral + 1);
-  }
-  const addBad = () => {
-    setBad(bad + 1);
-  }
-
-  return (
-    <div>
-      <Feedback onGood={() => addGood()} onNeutral={()=> addNeutral()} onBad={()=> addBad()} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+export default App
