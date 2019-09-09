ccb2905849f94c66b6a0ed4efb9420815e2d2734
Micheal Waldrup <waldrupm@gmail.com>
2019-09-09 13:06:08 -0400

    Refactor to phonebook

--- src/App.js
+++ src/App.js
@@ -1,57 +1,90 @@
 import React, { useState } from 'react'
-import Note from './components/Note'
+import Contact from './components/Contact'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes) 
-  const [newNote, setNewNote] = useState('')
+const App = () => {
+  const [contacts, setContacts] = useState([
+    { name:'Guy Fieri', number: '020-4837473'},
+    { name:'Goron Ramsay', number: '75749483832'},
+    { name:'Mr. Tasty', number: '43-4982839'},
+    { name:'Dude man', number: '11-33-448382'},
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [filterBy, setFilterBy] = useState('');
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+  const contactsToShow = showAll ? contacts : contacts.filter(contact => contact.name.toUpperCase.match(filterBy.toUpperCase) !== -1)
 
 
-  const rows = () => notesToShow.map(note =>
-    <Note
-      key={note.id}
-      note={note}
+  const rows = () => contactsToShow.map(contact =>
+    <Contact
+      key={contact.name}
+      name={contact.name}
+      number={contact.number}
     />
   )
   
-  const handleNoteChange = (event) => {
+  const handleContactNameChange = (event) => {
     console.log(event.target.value)
-    setNewNote(event.target.value)
+    setNewName(event.target.value)
   }
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5,
-      id: notes.length + 1,
-    }
+  const handleContactNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+    console.log(newNumber);
+  }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+  const handleFiltering = (event) => {
+    console.log(event.target.value)
+    setFilterBy(event.target.value)
+    if(event.target.value !== '') setShowAll(false)
+    console.log(filterBy);
+  }
+
+  const addContact = (event) => {
+    event.preventDefault()
+    // 
+    if(newName === '') return true
+    if(newNumber === '') return true
+
+    let dup = false
+    contacts.forEach(contact => {
+      if (contact.name === newName) dup = true
+    })
+
+    if (!dup) {
+      const contactObject = {
+        name: newName,
+        number: newNumber
+      }
+  
+      setContacts(contacts.concat(contactObject))
+      setNewName('')
+      setNewNumber('')
+    }
   }
 
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Phonebook</h1>
+      <form onSubmit={addContact}>
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          Show {showAll ? 'Only Important' : 'All'}
-        </button>
+        Filter:<input value={filterBy} onChange={handleFiltering}/>
       </div>
+        Name: <input
+          value={newName} 
+          onChange={handleContactNameChange}
+        /><br/>
+        Number: <input
+          value={newNumber} 
+          onChange={handleContactNumberChange}
+        /><br/>
+        <button type="submit">save</button>
+      </form>
       <ul>
         {rows()}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote} 
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
     </div>
   )
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = ({ name, number }) => {
+  return (
+    <li>{name}: {number}</li>
+  )
+}
+
+export default Contact
