06fb28094caba5ac12f604a089c477f59c86f66f
Micheal Waldrup <waldrupm@gmail.com>
2019-09-12 14:22:32 -0400

    Add note service

--- src/App.js
+++ src/App.js
@@ -17,11 +17,23 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
 
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important}
+
+    axios.put(url, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
+  }
+
 
   const rows = () => notesToShow.map(note =>
     <Note
       key={note.id}
       note={note}
+      toggleImportance={() => toggleImportanceOf(note.id)}
     />
   )
   
